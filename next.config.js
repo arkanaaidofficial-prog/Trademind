bb541d02d6c5173dd3ffc5261e9c09d0c3445a68
arkanaaidofficial-prog <arkanaa.id.official@gmail.com>
2026-06-08 08:27:51 +0700

    Stop ignoring build validation errors

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '*.supabase.co',
         pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        pathname: '/storage/v1/object/sign/**',
       },
     ],
   },
